cd3926790506edf63f7602bb41a75f267e874f97
park <foxman12@hanmail.net>
2025-03-15 17:13:21 +0900

    FirstProject push

---
+++ FirstProj/src/CharExample.java
@@ -0,0 +1,14 @@
+public class CharExample {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 0;
+		
+		b += a;	// b = b + a;
+		b *= a; // b = b * a;
+		
+		System.out.println(a);
+		System.out.println(b);
+	
+	}
+
+}
---
+++ FirstProj/src/VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+	public static void main(String[] args) {
+		int value = 10;
+		int result = value + 10;
+		byte var5 = (byte) 128;
+		
+		System.out.println(value);
+		System.out.println(result);
+		System.out.println(var5);
+	}
+}
