632a8c1e210700f63c4638a636994f82eea1ec55
park <foxman12@hanmail.net>
2025-03-16 17:43:01 +0900

    .

--- FirstProj/src/Main.java
+++ FirstProj/src/Main.java
@@ -1,10 +1,45 @@
+package q613;
+
+import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
-		int num;
-		num = -100;
-		
-		int num2 = -100;
-		
-		System.out.println(num);
+		Scanner sc = new Scanner(System.in);
+
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+
+		Student student = new Student();
+		student.setGrade(grade);
+		student.setName(name);
+		student.setSchool(school);
+
+		student.print();
+	}
+}
+
+class Student {
+	private String name;
+	private String school;
+	private int grade;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public void print() {
+		System.out.println("Name : " + name);
+		System.out.println("School : " + school);
+		System.out.println("Grade : " + grade);
 	}
 }
--- Jungol/src/struct/q613/Main.java
+++ Jungol/src/struct/q613/Main.java
@@ -6,17 +6,20 @@ class Person {
 	private String name;
 	private String school;
 	private int grade;
+	private int ban;
 
-	public Person(String name, String school, int grade) {
+	public Person(String name, String school, int grade, int ban) {
 		this.name = name;
 		this.school = school;
 		this.grade = grade;
+		this.ban = ban;
 	}
 
 	public void print() {
 		System.out.println("Name : " + name);
 		System.out.println("School : " + school);
 		System.out.println("Grade : " + grade);
+		System.out.println("Ban : " + ban);
 	}
 }
 
@@ -26,13 +29,23 @@ public class Main {
 		String name = sc.next();
 		String school = sc.next();
 		int grade = sc.nextInt();
-		sc.close();
+		int ban = sc.nextInt();
+
 
 //		System.out.println("Name : " + name);
 //		System.out.println("School : " + school);
 //		System.out.println("Grade : " + grade);
 		
-		Person p1 = new Person(name, school, grade);
+		Person p1 = new Person(name, school, grade, ban);
+		
+		name = sc.next();
+		school = sc.next();
+		grade = sc.nextInt();
+		ban = sc.nextInt();
+		sc.close();
+		
+		Person p2 = new Person(name, school, grade, ban);
 		p1.print();
+		p2.print();
 	}
 }
