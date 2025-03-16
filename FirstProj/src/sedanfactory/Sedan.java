a4f74198e599e05117de828c7e3109dcdc025e04
park <foxman12@hanmail.net>
2025-03-16 17:02:50 +0900

    Tire example

---
+++ FirstProj/src/sedanfactory/Sedan.java
@@ -0,0 +1,12 @@
+package sedanfactory;
+
+import component.Tire;
+
+public class Sedan {
+	public static void main(String[] args) {
+		Tire compoTire = new Tire();
+		compoTire.setSize(20);		
+
+		System.out.println(compoTire.getSize());
+	}
+}
