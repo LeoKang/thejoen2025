d64a0b1dc38233b7a8cbde13f6210cacf7fb0f4d
park <foxman12@hanmail.net>
2025-03-16 17:34:07 +0900

    struct 1

---
+++ Jungol/src/struct/q613/Main.java
@@ -0,0 +1,38 @@
+package struct.q613;
+
+import java.util.Scanner;
+
+class Person {
+	private String name;
+	private String school;
+	private int grade;
+
+	public Person(String name, String school, int grade) {
+		this.name = name;
+		this.school = school;
+		this.grade = grade;
+	}
+
+	public void print() {
+		System.out.println("Name : " + name);
+		System.out.println("School : " + school);
+		System.out.println("Grade : " + grade);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+		sc.close();
+
+//		System.out.println("Name : " + name);
+//		System.out.println("School : " + school);
+//		System.out.println("Grade : " + grade);
+		
+		Person p1 = new Person(name, school, grade);
+		p1.print();
+	}
+}
